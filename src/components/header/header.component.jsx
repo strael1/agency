715a16cd5a4bcc6e6986c0fd947cdd087d4fc1c7
Sergio Alves <80188778+serginhohalves@users.noreply.github.com>
2023-01-21 13:32:54 -0300

    nova branch tm101

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,36 +2,47 @@ import '../../styles/styles.scss';
 
 const Header = () => {
     return(
-        <header className="header">
-            <div className="header__container desktop">
-                <div className="header__container-1">
-                    <div className="header__logo">
-                        <img src="https://grupokpg.com/images/icons/logo/logowebp.webp" alt="logo" />
-                    </div>
-                    <div className="header__list">
-                        <ul className="header__main-list">
-                            <li className="header__item-list">Home</li>
-                            <li className="header__item-list">Sobre nós</li>
-                            <li className="header__item-list">Blog</li>
-                            <li className="header__item-list">Carreiras</li>
-                            <li className="header__item-list">Fale Conosco</li>
-                        </ul>
-                    </div>
-                </div>
+        <div>
+            {
+                window.innerWidth > 1024 ? 
+                    <header className="header">
+                        <div className="header__container">
+                            <div className="header__container-1">
+                                <div className="header__logo">
+                                    <img src="https://grupokpg.com/images/icons/logo/logowebp.webp" alt="logo" />
+                                </div>
+                                <div className="header__list">
+                                    <ul className="header__main-list">
+                                        <li className="header__item-list">Home</li>
+                                        <li className="header__item-list">Sobre nós</li>
+                                        <li className="header__item-list">Blog</li>
+                                        <li className="header__item-list">Carreiras</li>
+                                        <li className="header__item-list">Fale Conosco</li>
+                                    </ul>
+                                </div>
+                            </div>
 
-                <div className="header__container-2">
-                    <div className="header__col-1">
-                        <h2 className="title">Nos movemos<br/> por um propósito!</h2>
-                    </div>
-                    <div className="header__col-2">
-                        <p className="content">Inovação, crescimentos e desenvolvimento de grandes<br/> ideias é nossa rotina.</p>
-                    </div>
-                    <div className="header__col-3">
-                        <p className="paragraph">SAIBA MAIS</p>
-                    </div>                
-                </div>
-            </div>
-        </header>
+                            <div className="header__container-2">
+                                <div className="header__col-1">
+                                    <h2 className="title">Nos movemos<br/> por um propósito!</h2>
+                                </div>
+                                <div className="header__col-2">
+                                    <p className="content">Inovação, crescimentos e desenvolvimento de grandes<br/> ideias é nossa rotina.</p>
+                                </div>
+                                <div className="header__col-3">
+                                    <p className="paragraph">SAIBA MAIS</p>
+                                </div>                
+                            </div>
+                        </div>
+                    </header>
+                :
+
+                    <header>
+                            
+                    </header>
+            }
+        </div>
+        
     )
 }
 
